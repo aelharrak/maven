90e979b6aa15b30d757900746551bc562da0fc2a
Amine el harrak <amine.elharrak@gmail.com>
2016-11-29 15:33:07 +0000

    Add files via upload

---
+++ src/main/java/com/amine/app/App.java
@@ -0,0 +1,13 @@
+package com.amine.app;
+ 
+public class App 
+{
+    public int somme(int a, int b){
+        return a + b;
+    }
+     
+    public String printHello(String name){
+        return "Hello, "+ name;
+    }
+    
+}
